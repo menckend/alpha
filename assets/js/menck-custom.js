7770682f4fa240450132416662160da6edc46e31
u106598 <mencken.davidson@bcbsnc.com>
2023-11-22 01:03:24 -0500

    asdf

--- assets/js/menck-custom.js
+++ assets/js/menck-custom.js
@@ -1,18 +1,30 @@
 $(document).ready(function () {
-    $(".collapse").on("shown.bs.collapse", function () {
+
+    // Enable statefulness across pages/refreshes for manually-toggled sidebars
+
+    // When an item is shown (un-collapsed), create a local-storage item with a key of the item's ID (prepended by "coll_") and a value of true.
+        $(".collapse").on("shown.bs.collapse", function () {
         localStorage.setItem("coll_" + this.id, true);
     });
 
+    // When an item is collapsed/hidden, remove the related localStorage item (item ID prepended by "coll_")
     $(".collapse").on("hidden.bs.collapse", function () {
         localStorage.removeItem("coll_" + this.id);
     });
-    
+
+    // Make the state of the actual collapsible elements (and their control switches) match that of the localStorage items that track their state
     $(".collapse").each(function () {
         if (localStorage.getItem("coll_" + this.id) === "true") {
             $(this).collapse("show");
+            $(".menck-tn-toggle-control").each(function () {
+                $(this).attr("data-bs-target", "");
+                $(this).trigger("click");
+                $(this).attr("data-bs-target", "#MenckSidebar");
+                });
         }
         else {
             $(this).collapse("hide");
         }
     });
+
 });
