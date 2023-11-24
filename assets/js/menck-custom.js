74d25acf95c74763bde4940c3108b71c8797950d
u106598 <mencken.davidson@bcbsnc.com>
2023-11-23 23:25:34 -0500

    Update menck-custom.js

--- assets/js/menck-custom.js
+++ assets/js/menck-custom.js
@@ -18,22 +18,12 @@ $(document).ready(function () {
             $(this).collapse("show");
          }
         else {
-            if (this.id === "MenckSidebarToggle") {
                 $(this).collapse("hide");
                 $(".menck-tn-toggle-control").each(function () {
                     $(this).attr("data-bs-target", "");
                     $(this).trigger("click");
                     $(this).attr("data-bs-target", "#MenckSidebar");
                     });
-            }
-            if (this.id == "MenckRightbarToggle") {
-                $(this).collapse("hide");
-                $(".menck-tn-rb-toggle-control").each(function () {
-                    $(this).attr("data-bs-target", "");
-                    $(this).trigger("click");
-                    $(this).attr("data-bs-target", "#MenckRightbar");
-                    });
-            }
         }
     });
 
