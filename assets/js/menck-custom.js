30f323a5e9d9a9fd100591de1cd6bbfd66eb5694
u106598 <mencken.davidson@bcbsnc.com>
2023-11-25 15:20:26 -0500

    Update menck-custom.js

--- assets/js/menck-custom.js
+++ assets/js/menck-custom.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
                 var storagecomp = "coll_" + $(this).id;
                 var sidebarname = ("#" + $(this).id);
                 $(".menck-tn-toggle-control").each(function () {
-                    var controlname = $(this).data-bs-target;
+                    var controlname = this.data-bs-target;
                     console.log("matched storage key to sidebar name; values for sidebar and controller: " + controlname + " " + sidebarname);
                     if ((sidebarname === controlname.id)) {
                         $(this).trigger("click");
