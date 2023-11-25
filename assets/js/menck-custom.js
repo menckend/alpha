76d7c7693a9e0e417804ee445a8ca1608f25b7fd
u106598 <mencken.davidson@bcbsnc.com>
2023-11-25 12:26:56 -0500

    Update menck-custom.js

--- assets/js/menck-custom.js
+++ assets/js/menck-custom.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
-    // Things to do if this is a new browser session (synchronize sidebars and toggle-controls to shown/checked)
+    // Enable statefulness across pages/refreshes for manually-toggled sidebars
+    // When an item is shown (un-collapsed), create a local-storage item with a key of the item's ID (prepended by "coll_") and a value of true.
+    $(".collapse").on("shown.bs.collapse", function () {
+    sessionStorage.setItem("coll_" + this.id, true);
+    });
+    // When an item is collapsed/hidden, remove the related localStorage item (item ID prepended by "coll_")
+    $(".collapse").on("hidden.bs.collapse", function () {
+    sessionStorage.removeItem("coll_" + this.id);
+    });
+
+    // Things to do if this is an existing browser session (synchronize sidebars and toggle-controls to shown/checked)
     if (sessionStorage.getItem("menckenania-alpha-return-visitor") === "true") {
-        // Enable statefulness across pages/refreshes for manually-toggled sidebars
-        // When an item is shown (un-collapsed), create a local-storage item with a key of the item's ID (prepended by "coll_") and a value of true.
-            $(".collapse").on("shown.bs.collapse", function () {
-            sessionStorage.setItem("coll_" + this.id, true);
-        });
-        // When an item is collapsed/hidden, remove the related localStorage item (item ID prepended by "coll_")
-            $(".collapse").on("hidden.bs.collapse", function () {
-            sessionStorage.removeItem("coll_" + this.id);
-        });
         // Make the state of the actual collapsible elements (and their control switches) match that of the localStorage items that track their state
         $(".collapse").each(function () {
             if (sessionStorage.getItem("coll_" + this.id) === "true") {
@@ -25,11 +26,9 @@ $(document).ready(function () {
                     }
                 });
             }
-            else {
-                jstglctrl.collapse("hide");
-                }
         });
     }
+    // If this is a new-browser session, "click" all the sidebar toggle controls to "check" them and expand the associated sidebars 
     else {
         sessionStorage.setItem("menckenania-alpha-return-visitor", true);
         $(".menck-tn-toggle-control").each(function () {
