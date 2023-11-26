dd570089f3884e59bf8e68253a399501818ba41e
u106598 <mencken.davidson@bcbsnc.com>
2023-11-25 23:33:34 -0500

    Update menck-custom.js

--- assets/js/menck-custom.js
+++ assets/js/menck-custom.js
@@ -36,8 +36,7 @@ $(document).ready(function () {
         });
     }
 
-    console.log (document.title);
-    if (document.title === "| menckenania") {
+    if (document.title === " | menckenania") {
         $(".collapse").each(function () {
             if (this.id === "MenckenSidebar") {
             this.collapse("hide");
