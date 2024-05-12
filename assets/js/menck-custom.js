0a6a672b028f57455b13bf41bdda8028af14de7b
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-05-12 18:07:06 -0400

    adsfasdf

--- assets/js/menck-custom.js
+++ assets/js/menck-custom.js
@@ -1,27 +1,31 @@
 $(document).ready(function () {
     // Enable statefulness across pages/refreshes for manually-toggled sidebars
-    // When an item is shown (un-collapsed), create a local-storage item with a key of the item's ID (prepended by "coll_") and a value of true.
-    $(".collapse").on("shown.bs.collapse", function () {
-    sessionStorage.setItem("coll_" + this.id, true);
+
+    // When an item is shown (un-collapsed), create a local-storage item with a key of the item's ID (prepended by "visible_") and a value of true.
+    $(".collapse").on("show.bs.collapse", function () {
+    sessionStorage.setItem("show_" + this.id, true);
+    console.log ('An element was told to un-collapse/show-istelf. Tried to Write an entry to sessionStorage with this string in the key and value of true:', this.id);
     });
 
-    // When an item is collapsed/hidden, remove the related localStorage item (item ID prepended by "coll_")
-    $(".collapse").on("hidden.bs.collapse", function () {
-    sessionStorage.removeItem("coll_" + this.id);
+    // When an item is collapsed/hidden, set the related localStorage item (item ID prepended by "show_" to "false")
+    document.querySelectorAll(".menck-tn-toggle-control.collapse").on("hide.bs.collapse", function () {
+    sessionStorage.setItem("show_" + this.id, false);
     });
+
+    console.log ('Item collapse.  Set session-storage KVP to false for:', 'show_' + this.id);
 
     // Things to do if this is an existing browser session (synchronize sidebars and toggle-controls to shown/checked)
     if (sessionStorage.getItem("menckenania-alpha-return-visitor") === "true") {
         // Make the state of the actual collapsible elements (and their control switches) match that of the localStorage items that track their state
-        $(".collapse").each(function () {
-            var storagecomp = "coll_" + this.id;
-            if (sessionStorage.getItem(storagecomp) === "true") {
+        $document.querySelectorAll(".menck-tn-toggle-control.collapse").each(function () {
+            var storagecomp = "this_" + this.id;
+            if (sessionStorage.storagecomp === "true") {
                 $(this).collapse("show");
                 var sidebarname = "#" + this.id;
                 $(".menck-tn-toggle-control").each(function () {
                     var controlname = this.getAttribute('data-bs-target');
                     if ((sidebarname === controlname)) {
-                        $(this).trigger("click");
+                        $(this).trigger("click");   
                     }
                 });
             }
