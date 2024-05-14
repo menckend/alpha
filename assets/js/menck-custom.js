2abe742cedf3e73f590c9e925022040b2bafa8f5
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-05-14 00:11:45 -0400

    Update menck-custom.js

--- assets/js/menck-custom.js
+++ assets/js/menck-custom.js
@@ -1,67 +1,29 @@
 $(document).ready(function () {
     // Enable statefulness across pages/refreshes for manually-toggled sidebars
 
-
-
-//  start with elements collapsed.
-//
-// form input element's class +/- "collapsed" drives icon status.
-// 
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
-
-
-
-
-
-
-
-
-
-
-
-
     // Session-storage at page-load
     for (const [key, value] of Object.entries(sessionStorage)) {
         console.log('Contents of SessionStorage when page has loaded')
         console.log({key, value});
     };
 
-
-
     // When an item is shown (un-collapsed), create a local-storage item with a key of the item's ID (prepended by "visible_") and a value of true.
     $(".collapse").on("show.bs.collapse", function () {
     sessionStorage.setItem("show_" + this.id, true);
     console.log ('An element was told to un-collapse/show-istelf. Tried to Write an entry to sessionStorage with this string in the key and value of true:', this.id);
-
-    for (const [key, value] of Object.entries(sessionStorage)) {
-        console.log({key, value});
-    };
-
+        for (const [key, value] of Object.entries(sessionStorage)) {
+            console.log({key, value});
+        };
     });
 
     // When an item is collapsed/hidden, set the related localStorage item (item ID prepended by "show_" to "false")
     $(".collapse").on("hide.bs.collapse", function () {
     sessionStorage.setItem("show_" + this.id, false);
     console.log ('This thing tried to collapse/hide.Tried to Write an entry to sessionStorage with this string in the key and value of true:', this.id);
-
-    for (const [key, value] of Object.entries(sessionStorage)) {
-        console.log({key, value});
-    };
-
-   
+        for (const [key, value] of Object.entries(sessionStorage)) {
+            console.log({key, value});
+        };
     });
-
 
     // Things to do if this is an existing browser session (synchronize sidebars and toggle-controls to shown/checked)
     if ((sessionStorage.getItem("menckenania-alpha-return-visitor")) === "true") {
